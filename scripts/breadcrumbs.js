0bf7efed40554f1a0dc81ab4fa0f7a9d359dc895
Aňuška <demeterova.anna@gmail.com>
2015-05-08 21:27:25 +0200

    zmena

--- scripts/breadcrumbs.js
+++ scripts/breadcrumbs.js
@@ -2,6 +2,6 @@ if (typeof(Storage) != "undefined") {
     // Store
     localStorage.setItem("url", window.location.href);
     // Retrieve
-   alert(localStorage.getItem("url"));
+   //alert(localStorage.getItem("url"));
 } else {
 }
